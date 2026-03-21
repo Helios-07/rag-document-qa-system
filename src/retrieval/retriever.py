6113a48a564951771514e0d57181180493e1eaa3
Aman Nautiyal <amannautiyal794@gmail.com>
2026-03-22 02:07:29 +0530

    Implemented chunk retriever

---
+++ src/retrieval/retriever.py
@@ -0,0 +1,45 @@
+import pickle
+from src.embedding.embedding import EmbeddingModel
+from src.vector_store.faiss_store import FAISSVectorStore
+from utils.logger import get_logger
+from src.utils.exception import CustomException
+import sys
+
+logger=get_logger(__name__)
+
+class Retriever:
+    def __init__(self):
+        try:
+            logger.info("Initializing Retriever")
+
+            self.embeddng_model=EmbeddingModel()
+
+            with open("artifacts/chunks.pkl", 'rb') as f:
+                self.chunks=pickle.load(f)
+
+            logger.info(f"Loaded {len(self.chunks)} chunks")
+
+            dimension=384
+            self.vector_store=FAISSVectorStore(dimension)
+            self.vector_store.load()
+
+        except Exception as e:
+            logger.error("Error initializing retriever")
+            raise CustomException(e,sys)
+        
+    
+    def retrieve(self,query, top_k=3):
+        try:
+            logger.info(f"Retrieving for query: {query}")
+
+            query_embedding=self.embeddng_model.encode([query])[0]
+            distances,indices=self.vector_store.search(query_embedding, top_k)
+            res=[self.chunks[i] for i in indices[0]]
+
+            return res
+        
+        except Exception as e:
+            logger.error("Error during retrieval")
+            raise CustomException(e,sys)
+        
+
